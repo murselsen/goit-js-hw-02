2cf82ebbf4a5f714474d8a8b6913b964a227ee79
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2025-01-10 12:44:55 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,18 @@
 const getShippingCost = country => {
+  let price;
   switch (country) {
     case 'China':
       price = 100;
-      return `Shipping to ${country} will cost ${price} credits`;
     case 'Chile':
       price = 250;
-      return `Shipping to ${country} will cost ${price} credits`;
     case 'Australia':
       price = 170;
-      return `Shipping to ${country} will cost ${price} credits`;
     case 'Jamaica':
       price = 120;
-      return `Shipping to ${country} will cost ${price} credits`;
     default:
       return 'Sorry, there is no delivery to your country';
   }
+      return `Shipping to ${country} will cost ${price} credits`;
 };
 
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
